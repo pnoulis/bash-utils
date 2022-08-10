8b29c500e728e994f1649ca0a7b424458afcbb65
pavlos noulis <pavlos.noulis@gmail.com>
2022-08-11 00:27:25 +0300

    created isGitDir and isGitClean at git.utils.sh

--- git.utils.sh
+++ git.utils.sh
@@ -1,8 +1,85 @@
 #!/bin/bash
+# Author: Pavlos Noulis
+# Start Date: <2022-08-10 Wed>
 
 function isGitDir() {
-    [[ $# -eq 0 ]] && return 1
+    # input validation
+    ## 1. one parameter has been provided
+    [[ $# -eq 0 ]] && {
+        echo isGitDir: Wrong number of arguments
+        exit 1
+    }
+
+    ## 2. that paremeter is not an empty string
+    [ -z "$1" ] && {
+        echo isGitDir: Null string
+        exit 1
+    }
+
+    # make sure:
+    ## 1. is a path
+    ## 2. all nodes exist
+    ## 3. expand path
+    {
+        local tpath=$(realpath -qe "$1")
+    } || {
+        echo isGitDir: "$1": No such file or directory
+        exit 1
+    }
+
+    # check cwd is a git repo
+    pushd "$tpath" > /dev/null
+    {
+        git status >& /dev/null
+    } || {
+        echo isGitDir: "$tpath": Not a git repository
+        exit 1
+    }
+    popd > /dev/null
+    return 0
 }
 
-isGitDir hello
-echo $?
+function isGitClean() {
+    # input validation
+    ## 1. one parameter has been provided
+    [[ $# -eq 0 ]] && {
+        echo isGitClean: Wrong number of arguments
+        exit 1
+    }
+
+    ## 2. that paremeter is not an empty string
+    [ -z "$1" ] && {
+        echo isGitClean: Null string
+        exit 1
+    }
+
+    # make sure:
+    ## 1. is a path
+    ## 2. all nodes exist
+    ## 3. expand path
+    {
+        local tpath=$(realpath -qe "$1")
+    } || {
+        echo isGitClean: "$1": No such file or directory
+        exit 1
+    }
+
+
+    # check
+    pushd "$tpath" > /dev/null
+    ## 1. provided argument is a path repository
+    {
+        git status >& /dev/null
+    } || {
+        echo isGitClean: "$tpath": Not a git repository
+        exit 1
+    }
+    ## 2. working directory is clean
+    [ -n "$(git status --porcelain)" ] && {
+        echo isGitClean: "$tpath": working directory is dirty
+        exit 1
+    }
+    popd > /dev/null
+
+    return 0
+}
