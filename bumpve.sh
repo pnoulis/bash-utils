4a20c9e46b19d0085408a873532de0340210bc9b
pavlos noulis <pavlos.noulis@gmail.com>
2022-10-03 08:21:05 +0300

    bumpve.sh 1.0.0-alpha.2

--- bumpve.sh
+++ bumpve.sh
@@ -6,38 +6,44 @@
 
 
 # Options
+## -------------------------------------- ##
+VERSION=
 ELEMENT_TARGET=
 IDENTIFIER_TARGET=
+STITCH=
+QUIET=
 
+function usage {
+    cat <<EOF
+${0}: [OPTION]... version
+-c core
+-r release
+-b build
+-M major
+-m minor
+-p patch
+-s stitch
+-q quiet
+-h help
+EOF
+}
 
 # Utils
-
+# --------------------------------------- ##
 # Core[-PreRelease[+Build] | -PreRelease | +Build]
 PREFIX_RE="[vV]"
 CORE_RE="(?:0\.|[1-9]\d*\.){2}(?:0|[1-9]\d*)"
 RELEASE_RE="(?:(?:[-1-9][-0-9]*|[-a-zA-Z]*|0)\.)*(?:[-1-9][-0-9]*|[-a-zA-Z]*|0)"
 BUILD_RE="(?:[-a-zA-Z0-9]*\.)*[-a-zA-Z0-9]*"
 SEMVER_RE="^${PREFIX_RE}?${CORE_RE}(?:-${RELEASE_RE})?(?:\\+${BUILD_RE})?\$"
-
-# Bump algorithm
-ELEMENTS=
+MAX_IDENTIFIERS=3
 DATE_FMT=(
     [0]="%a-%d-%m-%Y"
     [1]="%Y%m%d"
     [2]="%d%m%Y"
     [3]="%a%d%m%Y"
 )
-
-
-function usage {
-    cat <<EOF
-${0}: -option version
--M major
--m minor
--p patch
--h help
-EOF
-}
+elements=
 
 function parse {
     PREFIX=$(echo "$VERSION" | sed -nE 's/^(v|V).*/\1/p')
@@ -57,7 +63,7 @@ function joinTokens {
 }
 
 function stateError {
-    echo should exit with error code
+    [[ $QUIET ]] && exit 1
     exit 1
 }
 
@@ -90,7 +96,24 @@ function bumpRelease {
     IFS="." read -a tokens <<< "$1"
     local major=("alpha" "beta" "rc")
     local i=0
-    local len=${#tokens[@]}
+
+    [[ -z $IDENTIFIER_TARGET || $STITCH ]] && i=0
+    # [[ ! $i ]] && i=$IDENTIFIER_TARGET
+
+    case $i in
+        0)
+            [[ ! ${tokens[0]} && $STITCH ]] && tokens[0]=${major[0]}
+            ;&
+        1)
+            [[ ! ${tokens[1]} && $STITCH ]] && tokens[1]=1
+            ;;
+        3)
+            [[ ! ${tokens[2]} && $STITCH ]] && tokens[2]=0
+        *)
+            stateError
+            ;;
+    esac
+
 
     if [[ -z $IDENTIFIER_TARGET ]]; then # empty
         tokens[0]=${major[0]}
@@ -109,6 +132,7 @@ function bumpRelease {
                 ;;
             *)
                 stateError
+                ;;
         esac
     fi
     joinTokens "${tokens[@]}"
@@ -149,11 +173,8 @@ function bump {
     return 0
 }
 
-while getopts ":hMmpcrba" o; do
+while getopts ":hcrbMmpsq" o; do
     case $o in
-        a) # auto
-            echo "stitch a semver to full specification "
-            ;;
         c) # core
             ELEMENT_TARGET=0
             ;;
@@ -172,28 +193,38 @@ while getopts ":hMmpcrba" o; do
         p) # patch
             IDENTIFIER_TARGET=2
             ;;
+        s) # stitch
+            STITCH=0
+            # completes missing parts if any of a semver according to provided
+            # release and build schemes
+            ;;
+        q) # quiet
+            QUIET=0
+            ;;
         h)
             usage
             exit 0
             ;;
         *)
-            stateError
+            usage > /dev/stderr
+            exit 1
             ;;
     esac
 done
 
 shift $((OPTIND - 1))
 
-VERSION="v1.0.0-alpha.1+build.1"
+#VERSION="v1.0.0-alpha.1+build.1"
+VERSION="v1.0.0+build.1"
 
-# Where +1 offsets the prefix at ELEMENTS[0]
+# Where +1 offsets the prefix at elements[0]
 ((ELEMENT_TARGET++))
 echo "VERSION: ${VERSION}"
 echo "ELEMENT TARGET: ${ELEMENT_TARGET}"
 echo "IDENTIFIER TARGET: ${IDENTIFIER_TARGET}"
 echo -----------------------------
 
-ELEMENTS=(
+elements=(
     [0]="$(grep -Po "${PREFIX_RE}" <<< "$VERSION")"
     [1]="$(grep -Po "${CORE_RE}" <<< "$VERSION")"
     [2]="$(grep -Po "\-${RELEASE_RE}\\+" <<< "$VERSION" | sed 's/[-+]//g')"
@@ -201,15 +232,17 @@ ELEMENTS=(
 )
 
 
-ELEMENTS[$ELEMENT_TARGET]=$(bump "${ELEMENTS[ELEMENT_TARGET]}" $ELEMENT_TARGET)
-unset IDENTIFIER_TARGET
+echo "${elements[@]}"
 
+elements[$ELEMENT_TARGET]=$(bump "${elements[ELEMENT_TARGET]}" $ELEMENT_TARGET)
+unset IDENTIFIER_TARGET
 ((ELEMENT_TARGET++))
+
 while [ $ELEMENT_TARGET -le 3 ]; do
     echo $ELEMENT_TARGET
-    ELEMENTS[$ELEMENT_TARGET]=$(bump "${ELEMENTS[$ELEMENT_TARGET]}" $ELEMENT_TARGET)
+    elements[$ELEMENT_TARGET]=$(bump "${elements[$ELEMENT_TARGET]}" $ELEMENT_TARGET)
     ((ELEMENT_TARGET++))
 done
 
-VERSION="${ELEMENTS[0]}${ELEMENTS[1]}-${ELEMENTS[2]}+${ELEMENTS[3]}"
+VERSION="${elements[0]}${elements[1]}-${elements[2]}+${elements[3]}"
 echo "$VERSION"
